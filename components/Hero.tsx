74737db7b3424fb25f422dc33c4078152fcf9109
Cardano G.O.A.T <Jrkbender@gmail.com>
2022-10-06 21:24:08 -0400

    updating typewriter text size on mobile

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -36,7 +36,7 @@ function Hero({}: Props) {
         
         <div className='z-20'>
             <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>Software Developer</h2>
-            <h1 className='text-5xl lg:text-6xl font-semibold px-10'>
+            <h1 className='text-2xl md:text-5xl lg:text-6xl font-semibold px-10'>
                 <span className='mr-3'>{text}</span>
                 <Cursor cursorColor='#47FF09' />
             </h1>
